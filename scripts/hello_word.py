f70c3af3bfdf3abd57f83f4a745bffd536405682
Julia Batista <juliabatista1@hotmail.com>
2026-03-04 10:41:50 -0600

    added math operations

--- scripts/hello_word.py
+++ scripts/hello_word.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+# prompt the user for their name
+name =input("Please enter your name: ")
+
+#two ways to priint "Hello, world!"
 print("Hello, world!")
 print("hello, world!")
+
+#personalized greeting, using the name from input() above
+print("Hello, " + name + "I hopee you have a great day!")
+
+#some math
+print(7+7)
+
