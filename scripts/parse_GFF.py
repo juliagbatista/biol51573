57a226beebf60acc8e01d42aeadfd3a17966ec0f
Julia Batista <juliabatista1@hotmail.com>
2026-04-15 10:02:21 -0500

    added import argparse in the parse script

---
+++ scripts/parse_GFF.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#exercise 1: Write a Python script called parse_GFF.py that will contain the main() function of your script, call and run it from the command line.
+#Use the argparse() module to accept two command line arguments: name of the genome FASTA file and name of the GFF file
+
+
+#import packages in the very top of the script
+import argparse
+
+
